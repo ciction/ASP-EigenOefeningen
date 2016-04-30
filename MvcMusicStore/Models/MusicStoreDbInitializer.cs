c3aae7ffb3ef19660a2760c468843109a8f1fe87
Christophe Swolfs <christophe.swolfs@student.ehb.be>
2016-04-30 11:58:05 +0200

    first commit

---
+++ MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer: System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.Add(new Artist { Name = "Bonobo" });
+            context.Genres.Add(new Genre { Name = "DownTempo" });
+            context.Albums.Add(new Album {
+                                            Artist = new Artist { Name = "Imogen heap" },
+                                            Genre = new Genre { Name = "Indie" },
+                                            Price = 9.99m,
+                                            Title = "Speak for yourself"
+                                        });
+            base.Seed(context);
+        }
+    }
+}
